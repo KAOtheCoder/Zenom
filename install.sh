6769328b52f9fac7cacdc897420fd578cb698d83
Samet Gul <asam.gul@gmail.com>
2020-09-10 15:59:41 +0300

    [INSTALL] fix installation script. Add ubuntu dependencies as commnet

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 #qt4 and boost should be present in your system
+
+#arch setup
 #pacaur -S qt4
 #pacaur -S boost
 
+#ubunutu setup
+#sudo apt-get install libboost-all-dev
+#sudo apt-get install qt4-qmake qt4-default
+
 #build osg
 cd OpenSceneGraph
-sudo rm -fr build
-sudo mkdir build 
+rm -fr build
+mkdir build 
 cd build
 cmake -DDESIRED_QT_VERSION=4 ../
 make osgQt osgSim osgdb_osg osgdb_deprecated_osg -j4
@@ -40,7 +46,9 @@ cd zenom
 qmake-qt4 .
 make -j4
 sudo make install
+cd zenom/examples
+make
 
 #add ld library path to bashrc
-#echo 'export LD_LIBRARY_PATH=/usr/local/qwt-6.0.1-svn/lib/:/usr/local/lib64:/usr/local/osg/lib' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/qwt-6.0.1-svn/lib/:/usr/local/lib64:/usr/local/osg/lib' >> ~/.bashrc
 
